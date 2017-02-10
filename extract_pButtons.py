fb4190001fc5098cdee062ffbcaa78ff5dfba5e9
murrayo <murray.oldfield@intersystems.com>
2017-02-10 21:16:15 +1000

    Add files

---
+++ extract_pButtons.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+"""Chop up key sections of pButtons and write to formatted .csv files.
+    
+Usage:
+    
+   extract_pButtons.py </pButtons/file/name>  
+    
+    Example: 
+        extract_pButtons.py ./pB_test_24hours_30sec.html
+"""
+import argparse
+import sys
+import csv
+import os
+import shutil
+
+
+def get_section(SearchFileName, outputFileName, startString, endString, os_details):
+    """Extracts blocks (eg vmstat). Trusts that pButtons has blocks of output between two key words.
+    
+    Args:
+        SearchFileName: path and file name to search.
+        outputFileName: output file name
+        startString: tag to start extract
+        endString: tag to start extract
+        
+        
+    Output:
+        Outputs text file of block of lines with context specific fomating.
+    """ 
+    with open(SearchFileName, mode='rt', encoding='ISO-8859-1') as infile, \
+         open(outputFileName, mode='wt') as outfile:
+        # Note: I had to use ISO-8859-1 encoding NOT std utf-8.  
+        # I suspect characters from non-english, Thai for example. You can also try utf-8.
+        
+        copy = False
+        for line in infile:
+        
+            if startString in line:
+                copy = True
+                
+                if startString == 'beg_mgstat':
+                    infile.readline() # skip first line of junk
+                    
+                if startString == 'beg_vmstat': # write header
+                    if os_details == 'RH':
+                        line = ' '.join(line.split()) + '\n' # strip leading and multiple spaces
+
+                        headerLine = line.split('<pre> ',1)[1] # get right hand part of ugly line            
+                        headerLine = ''.join(headerLine) # convert part to a string                              
+                        headerLine = headerLine.split(' ') # split to change column headings
+                        headerLine[0]='Date'
+                        headerLine[1]='Time'
+                    
+                        outfile.write(' '.join(headerLine))
+                    else:
+                        # TBD
+                        pass
+                        
+                        
+            elif endString in line:     
+                if copy == True:
+                    break             # only ever one block (even iostat)
+                    # copy = False    # To make more generic with multiple blocks
+                
+            elif copy: 
+                if startString == 'beg_mgstat':
+                   line = line.replace(' ', '') # strip spaces
+                if startString == 'beg_vmstat' or startString == 'id=iostat' :
+                    line = ' '.join(line.split()) + '\n' # strip leading and multiple spaces
+                
+                outfile.write(line)
+    
+
+def csv_converter(infile, outfile):
+    """
+    Read input text file and write out to new csv file.
+    """
+    
+    in_txt = csv.reader(open(infile, mode='rt'), delimiter = ' ')
+    out_csv = csv.writer(open(outfile, mode='wt'))
+
+    out_csv.writerows(in_txt)
+
+    
+def split_iostat(inputListFile, inputDataFile):
+    """ 
+    Split preprocessed iostat into one text file per disk.
+    
+    Args:
+        inputListFile: file containing list of disks (first block of iostat).
+        inputDataFile: full iostat file.
+        
+    Output:
+        Writes a csv file for each disk.
+    """
+    
+    csv_converter(inputListFile, 'list_iostat.csv')
+    csv_converter(inputDataFile, 'all_iostat.csv')
+    
+    # For each disk write a separate output file
+    with open('list_iostat.csv', mode='rt') as inlistfile:
+        disklist = []
+        
+        for line in inlistfile:
+            disklist.append(line.split(','))
+        
+        for disk in disklist:
+            header = False   
+            
+            with open('all_iostat.csv', mode='rt') as indatafile, \
+                 open('iostat_' + disk[0] + '.csv', mode='wt') as outfile:       
+                
+                for diskline in indatafile: 
+                
+                    if 'Device:' in diskline and header == False:
+                        header = True
+                        outfile.write(diskline) 
+                
+                    if disk[0] in diskline:
+                        outfile.write(diskline) 
+    
+    
+def detect_pB_os(SearchFileName):
+    """
+    Detect OS from Caché version string.
+    """
+    
+    with open(SearchFileName, mode='rt', encoding='ISO-8859-1') as infile:
+
+        for line in infile:
+        
+            if 'Version String:' in line:
+                
+                if 'AIX' in line:
+                    pB_os = 'AIX'
+                elif 'Itanium' in line:  
+                    pB_os = 'Itanium'
+                elif 'AIX' in line:  
+                    pB_os = 'AIX'
+                elif 'Red Hat Enterprise Linux' in line:  
+                    pB_os = 'RH'
+                elif 'Windows' in line:
+                    pB_os = 'Windows'
+                else:            
+                    pB_os = 'undefined'    
+                    
+                break             # bail
+    
+    return pB_os           
+                  
+
+def mainline(SearchFileName):
+
+    """Step through pButtons file gathering blocks of text and processing.
+    
+    Args: 
+        SearchFileName = path and name of pButtons file.
+    
+    Output: 
+        outputs fomatted csv files for available pButtons sections.
+    """
+
+    # get os details   
+    os_details = detect_pB_os(SearchFileName)
+    
+    # mgstat
+    get_section(SearchFileName, 'mgstat.csv', 'beg_mgstat', 'end_mgstat', os_details)
+    
+
+    if os_details == 'RH':
+    
+        # vmstat - needs to be converted to csv
+        get_section(SearchFileName, 'vmstat.txt', 'beg_vmstat', 'end_vmstat', os_details)
+        csv_converter('vmstat.txt', 'vmstat.csv')
+    
+        # iostat is messy, strip down and separate by disk in several steps
+        get_section(SearchFileName, 'iostat_tmp.txt', 'id=iostat', '#Topofpage', os_details)
+        get_section('iostat_tmp.txt', 'iostat_list.txt', 'Device:', ':', os_details)   
+        split_iostat('iostat_list.txt','iostat_tmp.txt')
+    
+        # sar is also messy, strip down and separate by disk in several steps
+        get_section(SearchFileName, 'sar_tmp.txt', 'id=sar-d', 'Average:', os_details) 
+        #To Do - homework ;)            
+    
+        # tidy up, comment for debugging
+        os.remove('vmstat.txt')
+        os.remove('iostat_list.txt')
+        os.remove('iostat_tmp.txt')
+        os.remove('sar_tmp.txt')
+        os.remove('list_iostat.csv')    
+    
+    elif os_details == 'Windows':
+        get_section(SearchFileName, 'win_perfmon.csv', 'beg_win_perfmon', 'end_win_perfmon', os_details)
+    
+    else:
+        # Still work to do on other OS's.
+        print('Only mgstat supported on ' + os_details)
+          
+    
+    # move csv files somewhere
+    os.makedirs('./metrics', exist_ok = True)
+
+    for checkFile in ['vmstat.csv', 'mgstat.csv', 'win_perfmon.csv']:
+        if os.path.isfile(checkFile):
+            os.rename(checkFile, './metrics/' + checkFile)
+
+    if os.path.isfile('all_iostat.csv'):        
+        os.makedirs('./metrics/iostat', exist_ok = True)
+        os.rename('all_iostat.csv', './metrics/all_iostat.csv')
+
+        files = os.listdir('.')
+        for f in files:
+            if (f.startswith('iostat_')):
+                shutil.move(f, './metrics/iostat/' + f)
+    
+                
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Extract useful metrics from pButtons to csv files')
+    parser.add_argument("pButtons_file_name", help="Path and pButtons file name to extract")
+    args = parser.parse_args()
+ 
+    try:
+        mainline(args.pButtons_file_name)
+    except OSError as e:
+        print('Could not process pButtons file because: {}'.format(str(e)))
+    
---
+++ graph_pButtons.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+"""Graph input file.
+    
+Usage:
+    
+   graph_pButtons.py </input/file/name.csv>  
+    
+    Example: 
+        graph_pButtons.py ./pB_test_24hours_30sec.html
+"""
+import argparse
+import sys
+import os
+import shutil
+
+import math
+import pandas as pd
+
+import mpl_toolkits.axisartist as AA
+from mpl_toolkits.axes_grid1 import host_subplot
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+from matplotlib.dates import DateFormatter
+
+
+def graph_column(CsvFileName, InterestingColumns):
+
+    file_prefix = (os.path.basename(CsvFileName)).split('.')[0]
+
+    data = pd.read_csv(
+        CsvFileName, 
+        header=0,
+        parse_dates=[[0,1]]
+       )
+
+    data.info()
+
+    data.columns=data.columns.str.strip()
+    data=data.rename(columns={'Date_Time':'DateTime'})
+
+    data.index=data.DateTime
+    
+    for ColumnName in InterestingColumns:
+        print(ColumnName)
+        
+        for graph_style in ['dots', 'lines']:
+    
+            plt.figure(num=None, figsize=(10,6), dpi=80, facecolor='w', edgecolor='dimgrey')
+            
+            if graph_style == 'dots':
+                plt.plot(data.DateTime, data[ColumnName], ".", markersize=2, color='dimgrey')
+            else:    
+                plt.plot(data.DateTime, data[ColumnName], color='dimgrey')
+            
+            plt.grid()
+    
+            plt.title(ColumnName + " of %s" %(CsvFileName), fontsize=10)
+            plt.ylabel(ColumnName, fontsize=10)
+        
+            plt.xlabel("Time", fontsize=10)
+
+            ax = plt.gca()
+            ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+
+            #plt.show()
+            plt.savefig(file_prefix  + '_' + ColumnName + '_' + graph_style + '.png')
+            plt.close('all')
+
+def mainline(CsvFileName, CsvFileType):
+
+    """Chart input file.
+    
+    Args: 
+        CsvFileName = path and name of csv file.
+    
+    Output: 
+        outputs graphs files for specific columns.
+    """
+    
+    if CsvFileType == "mgstat" :
+        InterestingColumns = ['Glorefs', 'RemGrefs', 'PhyRds', 'Rdratio', 'Gloupds', 'RouLaS', 'PhyWrs', 'WDQsz', 'WDphase', 'Jrnwrts', 'BytSnt', 'BytRcd', 'WIJwri', 'RouCMs', 'Rourefs', 'WDtmpq']
+    elif CsvFileType == "vmstat" :
+        InterestingColumns = ['r','b','swpd','free','us','sy','id','wa']
+    else:
+        pass
+    
+    graph_column(CsvFileName, InterestingColumns)
+
+    
+    # move files somewhere
+    os.makedirs('./charts', exist_ok = True)
+
+    files = os.listdir('.')
+    for f in files:
+        if (f.endswith('.png')):
+            shutil.move(f, './charts/' + f)
+
+    
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Graph metrics from pButtons csv files')
+    parser.add_argument("csv_file_name", help="Path and csv file name to graph")
+    parser.add_argument("csv_file_type", help="File type, e.g. mgstat or vmstat")
+    args = parser.parse_args()
+ 
+    try:
+        mainline(args.csv_file_name, args.csv_file_type)
+    except OSError as e:
+        print('Could not process csv file because: {}'.format(str(e)))
+    
