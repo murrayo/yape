cec0a5bc52f3cf5189fb0b06caf9c59d14e0d7e0
Murray Oldfield <murray.oldfield@intersystems.com>
2017-03-09 17:57:42 +1000

    Tidy as per pyCharm recommendations

--- extract_pButtons.py
+++ extract_pButtons.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 """Chop up key sections of pButtons and write to formatted .csv files.
-    
-Usage:
-    
-   extract_pButtons.py </pButtons/file/name>  
-    
-    Example: 
-        extract_pButtons.py ./pB_test_24hours_30sec.html
         
 """
 import argparse
@@ -24,78 +17,80 @@ def get_section(SearchFileName, outputFileName, startString, endString, os_detai
         outputFileName: output file name
         startString: tag to start extract
         endString: tag to start extract
-        
+        os_details: what operating system
+
         
     Output:
-        Outputs text file of block of lines with context specific fomating.
-    """ 
+        Outputs text file of block of lines with context specific formatting.
+    """
     with open(SearchFileName, mode='rt', encoding='ISO-8859-1') as infile, \
-         open(outputFileName, mode='wt') as outfile:
+            open(outputFileName, mode='wt') as outfile:
         # Note: I had to use ISO-8859-1 encoding NOT std utf-8.  
         # I suspect characters from non-english, Thai for example. You can also try utf-8.
-        
+
         copy = False
         for line in infile:
-        
+
             if startString in line:
                 copy = True
-                
+
                 if startString == 'beg_mgstat':
-                    infile.readline() # skip first line of junk
-                    
-                if startString == 'beg_vmstat': # write header
+                    infile.readline()  # skip first line of junk
+
+                if startString == 'beg_vmstat':  # write header
                     if os_details == 'RH':
-                        line = ' '.join(line.split()) + '\n' # strip leading and multiple spaces
+                        line = ' '.join(line.split()) + '\n'  # strip leading and multiple spaces
 
-                        headerLine = line.split('<pre> ',1)[1] # get right hand part of ugly line            
-                        headerLine = ''.join(headerLine) # convert part to a string                              
-                        headerLine = headerLine.split(' ') # split to change column headings
-                        headerLine[0]='Date'
-                        headerLine[1]='Time'
-                    
-                        outfile.write(' '.join(headerLine))
-                    
+                        header_line = line.split('<pre> ', 1)[1]  # get right hand part of ugly line            
+                        header_line = ''.join(header_line)  # convert part to a string                              
+                        header_line = header_line.split(' ')  # split to change column headings
+                        header_line[0] = 'Date'
+                        header_line[1] = 'Time'
+
+                        outfile.write(' '.join(header_line))
+
                     if os_details == 'AIX':
-                        line = ' '.join(line.split()) # strip leading and multiple spaces
+                        line = ' '.join(line.split())  # strip leading and multiple spaces
 
-                        headerLine = line.split('<pre> ',1)[1] # get right hand part of ugly line            
-                        headerLine = ''.join(headerLine) # convert part to a string 
-                        headerLine = headerLine.replace('sy','sy_call',1) # who thought duplicate column headings is a good idea?
-                        headerLine = headerLine.replace('hr mi se','')
-                        headerLine = headerLine + 'Time' + '\n'
-                        headerLine = headerLine.split(' ')
-                        
-                        outfile.write(' '.join(headerLine))
-    
+                        header_line = line.split('<pre> ', 1)[1]  # get right hand part of ugly line            
+                        header_line = ''.join(header_line)  # convert part to a string 
+                        header_line = header_line.replace('sy', 'sy_call',
+                                                          1)  # who thought duplicate column headings is a good idea?
+                        header_line = header_line.replace('hr mi se', '')
+                        header_line = header_line + 'Time' + '\n'
+                        header_line = header_line.split(' ')
+
+                        outfile.write(' '.join(header_line))
+
                     else:
                         # TBD
                         pass
-                                              
-            elif endString in line:     
-                if copy == True:
-                    break             # only ever one block (even iostat)
+
+            elif endString in line:
+                if copy:
+                    break  # only ever one block (even iostat)
                     # copy = False    # To make more generic with multiple blocks
-                
-            elif copy: 
+
+            elif copy:
                 if startString == 'beg_mgstat':
-                   line = line.replace(' ', '') # strip spaces
-                if startString == 'beg_vmstat' or startString == 'id=iostat' :
-                    line = ' '.join(line.split()) + '\n' # strip leading and multiple spaces
-                
+                    line = line.replace(' ', '')  # strip spaces
+                if startString == 'beg_vmstat' or startString == 'id=iostat':
+                    line = ' '.join(line.split()) + '\n'  # strip leading and multiple spaces
+
                 outfile.write(line)
-    
+
 
 def csv_converter(infile, outfile):
     """
     Read input text file and write out to new csv file.
     """
-    
-    in_txt = csv.reader(open(infile, mode='rt'), delimiter = ' ')
+
+    in_txt = csv.reader(open(infile, mode='rt'), delimiter=' ')
     out_csv = csv.writer(open(outfile, mode='wt'))
 
     out_csv.writerows(in_txt)
 
-    
+
 def split_iostat(inputListFile, inputDataFile):
     """ 
     Split preprocessed iostat into one text file per disk.
@@ -107,122 +102,122 @@ def split_iostat(inputListFile, inputDataFile):
     Output:
         Writes a csv file for each disk.
     """
-    
+
     csv_converter(inputListFile, 'list_iostat.csv')
     csv_converter(inputDataFile, 'all_iostat.csv')
-    
+
     # For each disk write a separate output file
     with open('list_iostat.csv', mode='rt') as inlistfile:
         disklist = []
-        
+
         for line in inlistfile:
             disklist.append(line.split(','))
-        
+
         for disk in disklist:
-            header = False   
-            
+            header = False
+
             with open('all_iostat.csv', mode='rt') as indatafile, \
-                 open('iostat_' + disk[0] + '.csv', mode='wt') as outfile:       
-                
-                for diskline in indatafile: 
-                
-                    if 'Device:' in diskline and header == False:
+                    open('iostat_' + disk[0] + '.csv', mode='wt') as outfile:
+
+                for disk_line in indatafile:
+
+                    if 'Device:' in disk_line and not header:
                         header = True
-                        outfile.write(diskline) 
-                               
-                    if disk[0] in diskline:
-                        outfile.write(diskline) 
-    
-    
-def detect_pB_os(SearchFileName):
+                        outfile.write(disk_line)
+
+                    if disk[0] in disk_line:
+                        outfile.write(disk_line)
+
+
+def detect_pbuttons_os(SearchFileName):
     """
     Detect OS from Caché version string.
+    :param SearchFileName:
+    :return: operating system
     """
-    
+
+    pbuttons_os = 'undefined'
+
     with open(SearchFileName, mode='rt', encoding='ISO-8859-1') as infile:
 
         for line in infile:
-        
+
             if 'Version String:' in line:
-                
+
                 if 'AIX' in line:
-                    pB_os = 'AIX'
-                elif 'Itanium' in line:  
-                    pB_os = 'Itanium'
-                elif 'AIX' in line:  
-                    pB_os = 'AIX'
-                elif 'Red Hat Enterprise Linux' in line:  
-                    pB_os = 'RH'
+                    pbuttons_os = 'AIX'
+                elif 'Itanium' in line:
+                    pbuttons_os = 'Itanium'
+                elif 'AIX' in line:
+                    pbuttons_os = 'AIX'
+                elif 'Red Hat Enterprise Linux' in line:
+                    pbuttons_os = 'RH'
                 elif 'Windows' in line:
-                    pB_os = 'Windows'
-                else:            
-                    pB_os = 'undefined'    
-                    
-                break             # bail
-    
-    return pB_os           
-                  
+                    pbuttons_os = 'Windows'
+                else:
+                    pbuttons_os = 'undefined'
+
+                break  # bail
+
+    return pbuttons_os
+
 
 def mainline(SearchFileName):
-
     """Step through pButtons file gathering blocks of text and processing.
     
     Args: 
         SearchFileName = path and name of pButtons file.
     
     Output: 
-        outputs fomatted csv files for available pButtons sections.
+        outputs formatted csv files for available pButtons sections.
     """
 
     # get os details   
-    os_details = detect_pB_os(SearchFileName)
-    
+    os_details = detect_pbuttons_os(SearchFileName)
+
     # mgstat
     get_section(SearchFileName, 'mgstat.csv', 'beg_mgstat', 'end_mgstat', os_details)
-    
 
     if os_details == 'RH':
-    
+
         # vmstat - needs to be converted to csv
         get_section(SearchFileName, 'vmstat.txt', 'beg_vmstat', 'end_vmstat', os_details)
         csv_converter('vmstat.txt', 'vmstat.csv')
-    
+
         # iostat is messy, strip down and separate by disk in several steps
         get_section(SearchFileName, 'iostat_tmp.txt', 'id=iostat', '#Topofpage', os_details)
-        get_section('iostat_tmp.txt', 'iostat_list.txt', 'Device:', ':', os_details)   
-        split_iostat('iostat_list.txt','iostat_tmp.txt')
-    
+        get_section('iostat_tmp.txt', 'iostat_list.txt', 'Device:', ':', os_details)
+        split_iostat('iostat_list.txt', 'iostat_tmp.txt')
+
         # sar is also messy, strip down and separate by disk in several steps
-        get_section(SearchFileName, 'sar_tmp.txt', 'id=sar-d', 'Average:', os_details) 
-        #To Do - homework ;)            
-    
+        get_section(SearchFileName, 'sar_tmp.txt', 'id=sar-d', 'Average:', os_details)
+        # To Do - homework ;)            
+
         # tidy up, comment for debugging
         os.remove('vmstat.txt')
         os.remove('iostat_list.txt')
         os.remove('iostat_tmp.txt')
         os.remove('sar_tmp.txt')
-        os.remove('list_iostat.csv')    
-    
+        os.remove('list_iostat.csv')
+
     elif os_details == 'Windows':
         get_section(SearchFileName, 'win_perfmon.csv', 'beg_win_perfmon', 'end_win_perfmon', os_details)
-    
+
     elif os_details == 'AIX':
         get_section(SearchFileName, 'vmstat.txt', 'beg_vmstat', 'end_vmstat', os_details)
         csv_converter('vmstat.txt', 'vmstat.csv')
-        
+
         # AIX iostat is very messy, strip down and comma separate only
         get_section(SearchFileName, 'iostat_tmp.txt', 'id=iostat', '#Topofpage', os_details)
         csv_converter('iostat_tmp.txt', 'iostat_AIX.txt')
         os.remove('iostat_tmp.txt')
-        
-    
+
     else:
         # Still work to do on other OS's.
         print('Only mgstat supported on ' + os_details)
-          
-    
+
     # move csv files somewhere
-    os.makedirs('./' + FILEPREFIX + 'metrics', exist_ok = True)
+    os.makedirs('./' + FILEPREFIX + 'metrics', exist_ok=True)
 
     for checkFile in ['vmstat.csv', 'mgstat.csv', 'win_perfmon.csv', 'iostat_AIX.txt']:
         if os.path.isfile(checkFile):
@@ -230,28 +225,26 @@ def mainline(SearchFileName):
 
     if os.path.isfile('all_iostat.csv'):
         os.remove('all_iostat.csv')
-        
+
         files = os.listdir('.')
         for f in files:
-            if (f.startswith('iostat_')):
-                shutil.move(f, './' + FILEPREFIX + 'metrics/' + f)
-    
-                
+            if f.startswith('iostat_'):
+                shutil.move(f, "./" + FILEPREFIX + "metrics/" + f)
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Extract useful metrics from pButtons to csv files')
     parser.add_argument("pButtons_file_name", help="Path and pButtons file name to extract")
     parser.add_argument("-p", "--prefix", help="add prefix string for output directory")
     args = parser.parse_args()
-    
-    
+
     if args.prefix is not None:
         FILEPREFIX = args.prefix
     else:
         FILEPREFIX = ''
- 
+
     try:
         mainline(args.pButtons_file_name)
     except OSError as e:
         print('Could not process pButtons file because: {}'.format(str(e)))
-    
