4d8e6bf59f93fe653e1e4f7ec008e7ad1c60b6df
Fabian <fabian@fabianhaupt.de>
2018-05-02 22:42:29 -0400

    made perfmon columns selectable; moved perfmon legend outside of graph

--- scripts/perfmon_tab.py
+++ scripts/perfmon_tab.py
@@ -7,6 +7,7 @@ from bokeh.plotting import Figure
 from bokeh.models import (CategoricalColorMapper, HoverTool,
 						  ColumnDataSource, Panel,
 						  FuncTickFormatter, SingleIntervalTicker, LinearAxis)
+from bokeh.models import Legend
 from bokeh.models.widgets import (CheckboxGroup, Slider, RangeSlider,
 								  Tabs, CheckboxButtonGroup,
 								  TableColumn, DataTable, Select)
@@ -30,14 +31,25 @@ def perfmon_tab(db):
         mypal=[cm(1.*i/numlines) for i in range(numlines)]
         mypal=list(map(lambda x: colors.rgb2hex(x), mypal))
         col=0
+        legenditems=[]
         for key in src.data.keys():
             if key=='datetime':
                 continue
             l=key+" "
             col=col+1
-            p.line(perfmon.index.values,perfmon[key],line_width=1,alpha=0.8,name=key,legend=key,color=mypal[col])
+            cline=p.line(perfmon.index.values,perfmon[key],line_width=1,alpha=0.8,color=mypal[col])
+            legenditems+=[(key,[cline])]
         p.legend.click_policy="hide"
+        legend = Legend(items=legenditems, location=(0, -30))
+        p.add_layout(legend, 'below')
         return p
+
+    def update(attr, old, new):
+        perfmons_to_plot = [perfmon_selection.labels[i] for i in perfmon_selection.active]
+        new_src = make_dataset(perfmons_to_plot)
+        plot=make_plot(new_src)
+        #TODO:crude hack in lack of a better solution so far
+        layout.children[1]=plot
 
     #get data from DB, setup index
     cur=db.cursor()
@@ -46,10 +58,19 @@ def perfmon_tab(db):
         return None
     perfmon=pd.read_sql_query("select * from perfmon",db)
     perfmon.index=pd.to_datetime(perfmon['datetime'])
+    perfmon=perfmon.drop(['datetime'],axis=1)
     perfmon.index.name='datetime'
-    src=make_dataset(perfmon.columns.values)
-    plot = make_plot(src)
 
-    layout = row(plot)
+    perfmon_selection = CheckboxGroup(labels=list(perfmon.columns),
+                                      active = [0,5],width=300,height=1000)
+
+    perfmon_list=[perfmon_selection.labels[i] for i in perfmon_selection.active]
+    src=make_dataset(perfmon_list)
+
+    plot = make_plot(src)
+    perfmon_selection.on_change('active', update)
+    controls=WidgetBox(perfmon_selection,width=300,height=1000)
+
+    layout = row(controls,plot)
     tab = Panel(child=layout, title = 'perfmon')
     return tab
