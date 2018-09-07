44f1ca7a362abf9ad13d9c706762c47f98f84a78
Fabian <fabian@fabianhaupt.de>
2018-09-07 09:18:52 -0400

    fleshed out unit test parsing of files in yape-testdata; currently runs for 80s, will need to add some status msg

--- yape/tests/test_parsepbuttons.py
+++ yape/tests/test_parsepbuttons.py
@@ -2,8 +2,10 @@ from yape.main import fileout,fileout_splitcols,parse_args,yape2
 from yape.parsepbuttons import parsepbuttons
 
 import os
+import traceback
+import logging
 
-TEST_DIR="test-data"
+TEST_DIR="yape-testdata"
 TEST_RESULTS="testresults"
 #just to understand how tests work
 class TestParser():
@@ -21,10 +23,30 @@ class TestParser():
         args=parse_args(params)
         assert args.quiet
         assert args.all
+        params=['--mgstat','--vmstat','--iostat','--sard','--monitor_disk','--perfmon','some.html']
+        args=parse_args(params)
+        assert args.graphmgstat
+        assert args.graphvmstat
+        assert args.graphiostat
+        assert args.graphsard
+        assert args.monitor_disk
+        assert args.graphperfmon
 
     def test_db_parse(self):
         onlyfiles = [os.path.join(TEST_DIR, f) for f in os.listdir(TEST_DIR) if os.path.isfile(os.path.join(TEST_DIR, f))]
         test_param = { ''}
         for file in onlyfiles:
-            params=["--filedb",os.path.join(TEST_RESULTS,file.replace("html","db")),file],
+            if "html" not in file:
+                continue
+            logging.debug(file)
+            basename=os.path.basename(file)
+            db_file=os.path.join(TEST_RESULTS,basename+".db")
+            params=['--filedb',db_file,file]
+            logging.debug(params)
             args=parse_args(params)
+            try:
+                yape2(args)
+            except:
+                logging.debug("error while parsing:"+file)
+                logging.debug(traceback.format_exc())
+                assert False,"exception while parsing: "+file
